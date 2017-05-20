4d0d1046b5060c123d4f8ba7f50db605e7223c3e
Damian Jesionek <Damian.Jes@gmx.de>
2017-05-20 22:56:23 +0200

    Added small documentation

--- renderer.js
+++ renderer.js
@@ -8,6 +8,10 @@ var SerialPort = require("serialport")
 //################################################################
 // tag reader part
 //################################################################
+// metraTec QR15 reader is used
+// Doc: http://www.metratec.com/fileadmin/docs/en/documentation/Docu_QR15_v1-8.pdf
+// Protocol Guide: http://www.metratec.com/fileadmin/docs/en/documentation/metraTec_ISO_Protocol-Guide_3_6.pdf
+
 var onTagRead = function(uid) {}
 var port = new SerialPort("/dev/ttyUSB0", {
     baudRate: 115200,
@@ -16,8 +20,8 @@ var port = new SerialPort("/dev/ttyUSB0", {
 
 port.on('open', function() {
     console.log("Initializing")
-    port.write("SRI SS 100\r")
-    port.write("CNR INV ONT\r")
+    port.write("SRI SS 100\r") // Init of tagreader (metraTec QR15)
+    port.write("CNR INV ONT\r") // Starting continuous reading of ids from nearby tags
 });
 
 port.on('data', function(data) {
@@ -35,7 +39,7 @@ port.on('error', function(err) {
 window.onbeforeunload = function() {
     //close tag, end readings etc
     console.log("onbeforeunload")
-    port.write("BRK\r")
+    port.write("BRK\r") // stopping any continuous readings
 }
 
 //################################################################
